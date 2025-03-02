d7f0e37ff9f13c4fbf039b240b96b2238d730c0d
RandikaEdirisooriya <randikaedirisooriya@gmail.com>
2025-03-02 17:29:17 +0530

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Navbar } from './Navbar'
+import { useState } from 'react'
+
+export const Header = () => {
+    const [navOpen, setNavOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-950 to-zinc-900/0 ">
+      <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid grid-cols-[1fr,3fr,1fr]'>
+        <h1>
+            <a href="/" className='logo'>
+            <img src='/logo.png' alt='logo' width={40} height={40}></img>
+            </a> 
+        </h1>
+        <div className='relative md:justify-self-center' > 
+            <button className='menu-btn md:hidden' onClick={() => setNavOpen(!navOpen)}>
+            <span className='material-symbols-rounded'>{navOpen ? 'close' : 'menu'} </span>
+            </button>
+         <Navbar navOpen={navOpen} />
+        </div>
+            <a href="#contact" className="btn btn-secondary max-md:hidden md:justofy-self-end">
+            Contact Me
+
+            </a>
+      </div>
+    </header>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+import tailwindScrollbar from "tailwind-scrollbar";
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans':['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [tailwindScrollbar],
+};
